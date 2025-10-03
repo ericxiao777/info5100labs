c2f122e5ca78bbb5b457750e6e3c686728e2e0ad
ericxiao777 <ericzherenxiao@gmail.com>
2025-10-02 22:51:06 -0400

    Lab3_JavaSwing

---
+++ Lab3_JavaSwing/src/genderButtonGroup.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author sunyujia
+ */
+class genderButtonGroup {
+
+    static Object getSelection() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
